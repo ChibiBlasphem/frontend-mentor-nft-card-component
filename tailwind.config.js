43270dc5b6bf2444ec7cd68812acadf32b327ea3
Christopher Debove <christopher.debove@toucantoco.com>
2021-12-11 17:12:29 +0100

    integration with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          soft: 'hsl(215, 51%, 70%)',
+          cyan: 'hsl(178, 100%, 50%)',
+        },
+        neutral: {
+          'blue-3x': 'hsl(217, 54%, 11%)',
+          'blue-2x': 'hsl(216, 50%, 16%)',
+          blue: 'hsl(215, 32%, 27%)',
+        },
+      },
+    },
+    fontFamily: {
+      display: ['Outfit'],
+    },
+  },
+  plugins: [],
+};
